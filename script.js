2e61acb90914bd202d2ac41d335ec414e50143f7
Sergio Vergara <sergiovergara123@gmail.com>
2024-02-20 19:27:01 -0500

    checkForPalindrome function checks for empty user input when clicking button

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const div = document.getElementById("result");
+
+function checkForPalindrome(input) {
+    if (input === "") {
+        return alert("Please input a value");
+    }
+    div.replaceChild();
+}
+
+button.addEventListener('click', () => {
+    checkForPalindrome(input.value);
+    input.value = '';
+  });   
