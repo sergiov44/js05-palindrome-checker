c704625fcae5fc380f00e0692fb18f1671f8d367
Sergio Vergara <sergiovergara123@gmail.com>
2024-03-03 20:44:59 -0500

    checkForPalindrome function completed based on example code. Project completed

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const input = document.getElementById("text-input");
-const button = document.getElementById("check-btn");
-const div = document.getElementById("result");
+const userInput = document.getElementById('text-input');
+const checkPalindromeBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
 
-function checkForPalindrome(input) {
-    if (input === "") {
-        return alert("Please input a value");
-    }
-    div.replaceChild();
-}
+const checkForPalindrome = input => {
+  const originalInput = input;
 
-button.addEventListener('click', () => {
-    checkForPalindrome(input.value);
-    input.value = '';
-  });
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
+
+  resultDiv.replaceChildren();
+
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${originalInput}</strong> ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome.`;
+
+  const pTag = document.createElement('p');
+  pTag.className = 'user-input';
+  pTag.innerHTML = resultMsg;
+  resultDiv.appendChild(pTag);
+
+  resultDiv.classList.remove('hidden');
+};
+
+checkPalindromeBtn.addEventListener('click', () => {
+  checkForPalindrome(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkForPalindrome(userInput.value);
+    userInput.value = '';
+  }
+});
